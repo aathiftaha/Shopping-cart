a12c3e2f18b5379e28a627a479d98ec14b996c61
Aathif Taha <132733686+aathiftaha@users.noreply.github.com>
2024-08-25 12:03:41 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,11 +9,11 @@ const Footer = () => {
          <div className="container">
             <div className="footer_logo"><a href="index.html"><img src="images/footer-logo.png" alt='Footer_Logo'/></a></div>
             
-            <div className='myInfo'>Developed & Designed by : MOHAMMED SHARIQUE</div>
+            <div className='myInfo'>Developed & Designed by : AATHIF TAHA</div>
 
-            <div className="myContacts"><FaMailBulk /> Mail : <a href="/">mdshariquek16@gmail.com</a></div>
+            <div className="myContacts"><FaMailBulk /> Mail : <a href="/">aathif.taha16@gmail.com</a></div>
 
-            <div className="myContacts"><BiSolidPhoneCall /> Contact : <a href="/">+91 9080 534 237</a></div>
+            <div className="myContacts"><BiSolidPhoneCall /> Contact : <a href="/">+91 9080 534 227</a></div>
          </div>
       </div>
     </div>
